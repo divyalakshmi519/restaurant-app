888afa4df9299cb3b833a005cc123179a1f687f7
Divya Lakshmi <divyalakshmi8818@gamil.com>
2025-08-06 14:35:08 +0000

    first commit

--- src/components/DishItem/index.js
+++ src/components/DishItem/index.js
@@ -28,41 +28,41 @@ const DishItem = ({
   }
 
   const renderControllerButton = () => (
-    <div className="controller-container d-flex align-items-center bg-success">
-      <button className="button" type="button" onClick={onDecreaseQuantity}>
+    <div className='controller-container d-flex align-items-center bg-success'>
+      <button className='button' type='button' onClick={onDecreaseQuantity}>
         -
       </button>
-      <p className="quantity">{getQuantity()}</p>
-      <button className="button" type="button" onClick={onIncreaseQuantity}>
+      <p className='quantity'>{getQuantity()}</p>
+      <button className='button' type='button' onClick={onIncreaseQuantity}>
         +
       </button>
     </div>
   )
 
   return (
-    <li className="mb-3 p-3 dish-item-container d-flex">
+    <li className='mb-3 p-3 dish-item-container d-flex'>
       <div
         className={`veg-border ${dishType === 1 ? 'non-veg-border' : ''} me-3`}
       >
         <div className={`veg-round ${dishType === 1 ? 'non-veg-round' : ''}`} />
       </div>
-      <div className="dish-details-container">
-        <h1 className="dish-name">{dishName}</h1>
-        <p className="dish-currency-price">
+      <div className='dish-details-container'>
+        <h1 className='dish-name'>{dishName}</h1>
+        <p className='dish-currency-price'>
           {dishCurrency} {dishPrice}
         </p>
-        <p className="dish-description">{dishDescription}</p>
+        <p className='dish-description'>{dishDescription}</p>
         {dishAvailability && renderControllerButton()}
         {!dishAvailability && (
-          <p className="not-availability-text text-danger">Not available</p>
+          <p className='not-availability-text text-danger'>Not available</p>
         )}
         {addonCat.length !== 0 && (
-          <p className="addon-availability-text">Customizations available</p>
+          <p className='addon-availability-text'>Customizations available</p>
         )}
       </div>
 
-      <p className="dish-calories text-warning">{dishCalories} calories</p>
-      <img className="dish-image" alt={dishName} src={dishImage} />
+      <p className='dish-calories text-warning'>{dishCalories} calories</p>
+      <img className='dish-image' alt={dishName} src={dishImage} />
     </li>
   )
 }
